dc757b4c432b0aa3616a1ae87aff4439a267990d
abu-yousuf1 <yousufadnan111@gmail.com>
2021-11-13 12:34:31 +0600

    implemant manage product

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# niche-website-server-side-Abu-yousuf1
+*  The Fragrance Family
+
+ * Live site Link: https://perfume-client.web.app/
+
+ * Client site github link: https://github.com/programming-hero-web-course-4/niche-website-client-side-Abu-yousuf1
+
+ * There is i implement 
+            * user authentication system,
+            * admin authentication , 
+            * error handling , 
+            * database 
+
+
--- index.js
+++ index.js
@@ -35,6 +35,14 @@ async function run() {
             res.send(result)
         })
 
+        //delete products..............
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(filter)
+            res.json(result)
+        })
+
         // insert product.........
         app.post('/product', async (req, res) => {
             const cursor = req.body;
